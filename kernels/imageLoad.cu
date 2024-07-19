3471498d3ee4c595f97f5f6c2ebdb583170a2454
Your Name <ctaylor7654@gmail.com>
2024-07-19 21:04:21 +0000

    loading image with cuda

--- kernels/imageLoad.cu
+++ kernels/imageLoad.cu
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
+#include <assert.h>
 #include "imageLoad.h"
+
+inline cudaError_t checkCuda(cudaError_t result)
+{
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+  return result;
+}
+
 __global__ void imageLoad(unsigned char* image, uchar4* imageLoaded, size_t imgSize) {
-    size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-    size_t stride = blockDim.x * gridDim.x;
-    for (; i < imgSize; i += stride) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if(i <imgSize) {
         imageLoaded[i].x = image[i * 4 + 0];
         imageLoaded[i].y = image[i * 4 + 1];
         imageLoaded[i].z = image[i * 4 + 2];
@@ -13,12 +24,22 @@ __global__ void imageLoad(unsigned char* image, uchar4* imageLoaded, size_t imgS
 }
 
 void imageLoadWrapper(unsigned char* image, uchar4* imageLoaded, size_t imgSize) {
-    cudaMallocManaged(&image, imgSize * sizeof(unsigned char));
-    cudaMallocManaged(&imageLoaded, imgSize * sizeof(uchar4));
-    int blockSize = 256;
-    int numBlocks = (imgSize + blockSize - 1) / blockSize;
-    imageLoad<<<numBlocks, blockSize>>>(image, imageLoaded, imgSize);
+    unsigned char* d_image;
+    uchar4* d_imageLoaded;
+
+    checkCuda(cudaMallocManaged(&d_image, imgSize * 4 * sizeof(unsigned char)));
+    checkCuda(cudaMallocManaged(&d_imageLoaded, imgSize * sizeof(uchar4)));
+    checkCuda(cudaMemcpy(d_image, image, imgSize * 4 * sizeof(unsigned char), cudaMemcpyHostToDevice));
+
+    int imgSizeInt = (int)imgSize;
+    int threadsPerBlock = 256;
+    int numBlocks = (imgSizeInt + threadsPerBlock - 1) / threadsPerBlock;
+    imageLoad<<<numBlocks, threadsPerBlock>>>(d_image, d_imageLoaded, imgSize);
+    checkCuda(cudaGetLastError());
     cudaDeviceSynchronize();
-    cudaFree(image);
-    cudaFree(imageLoaded);
+    for(int i=0;i<imgSize;i++) {
+        printf("%hhu\n", d_image[i].x);
+    }
+    cudaFree(d_image);
+    cudaFree(d_imageLoaded);
 }
--- main.cpp
+++ main.cpp
@@ -2,16 +2,42 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <string>
+#include <cstdlib> 
 #define STB_IMAGE_IMPLEMENTATION
 #include "include/stb_image.h"
 #include "kernels/imageLoad.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/stb_image_write.h"
 
 
+
+bool saveImage(const char* filename, uchar4* pixels, int width, int height) {
+    // Assuming uchar4 is a struct of 4 unsigned chars (RGBA)
+    // Convert uchar4* to unsigned char* required by stbi_write_png
+    std::cout << "inside save image" << std::endl;
+    unsigned char* data = new unsigned char[width * height * 4];    
+    for (int i = 0; i < width * height; ++i) {
+        std::cout << "R: " << pixels[i].x << std::endl;
+
+        data[i * 4 + 0] = pixels[i].x; // R
+        data[i * 4 + 1] = pixels[i].y; // G
+
+        data[i * 4 + 2] = pixels[i].z; // B
+        data[i * 4 + 3] = pixels[i].w; // A
+    }
+
+    // Write to file
+    int result = stbi_write_jpg(filename, width, height, 4, data, width * 4);
+
+    delete[] data; // Clean up the temporary array
+
+    return result != 0; // stbi_write_png returns 0 on failure, non-0 on success
+}
 
 uchar4* loadImage(const char* filename,int* width, int *height) {
     int channels;
     unsigned char* img = stbi_load(filename,width,height,&channels,STBI_rgb_alpha);
-    if (img = NULL) {
+    if (img == NULL) {
         std::cerr << "Error Loading Image: " << filename << std::endl;
         exit(1);
     }
@@ -20,15 +46,15 @@ uchar4* loadImage(const char* filename,int* width, int *height) {
 
     uchar4* d_image;
     imageLoadWrapper(img,d_image,imgSize);
+    stbi_image_free(img);
     return d_image;
 }
 
-
 int main(int, char**){
-    int* width;
-    int* height;
+    int width; 
+    int height;
+    uchar4* img = loadImage("/home/paperspace/CudaImageProcessing/image/thumb.gif",&width,&height);
     
-    uchar4* img = loadImage("image/deer.jpg",width,height);
-    std::cout << "width = " << *width << " height = " << *height << std::endl;
+    saveImage("/home/paperspace/CudaImageProcessing/image/deerNew.png",img,width,height);
     return 0;
 }
